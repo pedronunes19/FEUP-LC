6272d0a1cf7729d56e323dac9abfe475cb7d1fd5
pedrosilva17 <pedroafsuper@gmail.com>
2022-06-08 13:14:38 +0100

    Implemented piece vertical overflow, fixed floor collisions, simplified math

--- proj/src/game/game_state.c
+++ proj/src/game/game_state.c
@@ -3,6 +3,7 @@
 static tetromino_type piece_type[] = {I, J, L, S, O, T, Z};
 static int counter = 1;
 bool spawned = false;
+bool end = false;
 
 void start_game() {
   memset(*board, 0, sizeof(board));
@@ -12,23 +13,23 @@ void start_game() {
   place_tetromino();
 
   draw_game_ui();
-  for (int i = 0; i < 10; i++)
-    board[0][i] = 1;
   draw_board(board);
 }
 
-void place_tetromino() {
-  printf("Checking if piece can be drawn\n\n");
+bool check_space() {
+  //printf("Checking if piece can be drawn\n\n");
+  int last_y_idx = 0;
   for (int i = 0; i < 4; i++) {
     for (int j = 0; j < 4; j++) {
       if (tetromino->matrix[i][j] != 0) {
-        if (board[15 - i - tetromino->y][j + tetromino->x] != 0) { 
+        last_y_idx = i;
+        if ((board[15 - i - tetromino->y][j + tetromino->x] != 0) && ((i + tetromino->y) >= 0)) {
           /*
           printf("board y: %d\nboard x: %d\n", 15 - i - tetromino->y, j + tetromino->x);
           printf("board block value: %d\n\n", board[15 - i - tetromino->y][j + tetromino->x]);
           printf("Can't draw!\n\n");
           */
-          return;
+          return false;
         }
         else {
           /*
@@ -39,10 +40,24 @@ void place_tetromino() {
       }
     }
   }
+  //bottom piece block is outside of the board -> adjust failed, game over
+  if (last_y_idx + tetromino->y < 0) {
+    end = true;
+    return false;
+  }
+  return true;
+}
+
+void place_tetromino() {
+
+  while (!check_space() && !end) {
+    //Push the piece up trying to fit it in the remaining space
+    tetromino->y--;
+  }
   //printf("Drawing piece\n\n");
   for (int i = 0; i < 4; i++) {
     for (int j = 0; j < 4; j++) {
-      if (tetromino->matrix[i][j] != 0) {
+      if ((tetromino->matrix[i][j] != 0) && (i + tetromino->y >= 0)) {
         board[15 - i - tetromino->y][j + tetromino->x] = tetromino->type;
       }
     }
@@ -50,7 +65,7 @@ void place_tetromino() {
   /*
   printf("End of drawing piece\n\n");
   printf("Board state after drawing\n\n");
-  for (int i = 15; i > 0; i--) {
+  for (int i = 15; i >= 0; i--) {
     for (int j = 0; j < 10; j++) {
       printf("%d", board[i][j]);
     }
@@ -61,10 +76,12 @@ void place_tetromino() {
 }
 
 void piece_fall() {
+  //printf("\nThe end? %s", end ? "Yes!\n\n" : "Not yet...\n\n");
+  if (end) return;
   if (!spawned) clear_tetromino();
   /*
   printf("Board state after clear (only if piece can move)\n\n");
-  for (int i = 15; i > 0; i--) {
+  for (int i = 15; i >= 0; i--) {
       for (int j = 0; j < 10; j++) {
         printf("%d", board[i][j]);
       }
@@ -110,15 +127,18 @@ void draw_game_ui() {
 
 bool check_collision() {
   uint8_t y = tetromino->y + 1;
-  for (int i = 0; i < 4; i++)
+  for (int i = 0; i < 4; i++) {
     for (int j = 0; j < 4; j++) {
-      if (tetromino->matrix[i][j] != 0 && board[15 - i - y][j + tetromino->x] != 0) {
+      if ((tetromino->matrix[i][j] != 0) && (board[15 - i - y][j + tetromino->x] != 0)) {
         return true;
       }
-      if (tetromino->matrix[i][j] != 0 && y + (j) > 15) {
+      printf("\n\n%d - %d - %d\n\n", y + j, tetromino->matrix[i][j] != 0, y + (j) > 15);
+      if ((tetromino->matrix[i][j] != 0) && (y + (i) > 15)) {
         return true;
       }
     }
-
+    printf("\n");
+  }
+  printf("\n");
   return false;
 }
